aaec899d90a926fb3ccd67c56a68d46b4b457e86
Iury Souza <iury.pereira.souza@uel.br>
2022-03-23 14:04:30 -0300

    tentando gerar relatorio

--- app/routes/viewBCPatient.js
+++ app/routes/viewBCPatient.js
@@ -9,6 +9,8 @@ const User = db.User
 const BCPatient = db.BCPatient
 const Table = db.Table
 
+const client = require('@jsreport/nodejs-client')('http://localhost:3000')
+
 router.use(express.urlencoded({ extended: true }))
 
 router.get('/', async (req, res) => {
@@ -23,19 +25,37 @@ router.get('/', async (req, res) => {
     const labels = await utils.getLabels(p.show_data_for)
 
     res.render('../app/views/viewPatientTable', {
-        id_u: p.id_u, 
-        id_t: p.id_t, 
+        id_u: p.id_u,
+        id_t: p.id_t,
         db_user,
-        data, 
+        data,
         labels,
         title: utils.getPropertyName(p.show_data_for),
-        url_edit_user: '/home/editarUsuario?id=' + p.id_u, 
+        url_edit_user: '/home/editarUsuario?id=' + p.id_u,
         url_logout: '/home/logout'
     })
 })
 
 router.post('/filter', (req, res) => {
     res.redirect(`/BCPatient?p=${JSON.stringify(req.body)}`)
+})
+
+router.post('/report', (req, res) => {
+    async function render() {
+        const res = await client.render({
+            template: {
+                content: 'hello {{someText}}',
+                recipe: 'html',
+                engine: 'handlebars'
+            },
+            data: { someText: 'world!!' }
+        })
+
+        const bodyBuffer = await res.body()
+        console.log(bodyBuffer.toString())
+    }
+
+    render().catch(console.error)
 })
 
 module.exports = router
